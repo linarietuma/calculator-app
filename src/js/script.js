ff38a0b2bdcca1a3202ca239f2c7209d46e0140a
linarietuma <71130806+linarietuma@users.noreply.github.com>
2021-07-24 21:27:15 +0300

    Implement formatNumber(), fix comments

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,50 @@
 
-// track 
-let argument1 = [];
+// track user input
+let argument1   = [];
 const argument2 = [];
 let total = [];
-let operator = false;
-let float1 = false;
-let float2 = false;
+let operator    = false;
+let float1      = false;
+let float2      = false;
 let operatorType;
-let maxLength = 13;
 
 
-// change the number of digits displayed depending on screen size 
+// specify the max number of characters a user can enter
+let maxLength;
+const maxLengthMobile = 10;
+const maxLengthDesktop = 14;
+// specifies the number of max digits displayed based on the initial window size
+// initial window width corresponds to mobile design  
+if (window.innerWidth <= 550) {
+    maxLength = maxLengthMobile;
+}
+// initial window width corresponds to desktop design 
+else {
+    maxLength = maxLengthDesktop;
+}
+
+
+// change the max number of digits displayed depending on screen size 
 window.addEventListener('resize', () => {
-    console.log('dragged');
+    if (window.innerWidth <= 550) {
+        maxLength = maxLengthMobile;
+    } else {
+        maxLength = maxLengthDesktop;
+    }
 })
 
 // math functions
-const add = (a, b) => a + b;
+const add      = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide   = (a, b) => a / b;
+
 
 // collection of all button objects
 const btn = document.querySelectorAll('button');
-
-// div with the class if display
+// div with the class of display
 const displayed = document.querySelector('.display');
-// by default set the display to argument1
+// by default set the display to 0
 displayed.innerHTML = 0;
 
 // assign click event listener to each button 
@@ -62,13 +80,13 @@ const keyboard = {
 // add keydown event listener 
 document.addEventListener('keydown', e => {
     try {
-        // 
         displayed.innerHTML = clicked(keyboard[e.key]);
     }
     catch {
         console.log('Invalid key');
     }
 });
+
 
 // collection of input objects
 const toggle = document.querySelectorAll('input');
@@ -84,12 +102,14 @@ for (let i = 0; i < themeHref.length; i++) {
     });
 }
 
+
 // changes color of the selected operator button
-function selected() {
+function btnSelected() {
     // collection of button objects with the class operator
     const btnOperator = document.querySelectorAll('.operator');
 
     for (let i = 0; i < btnOperator.length; i++) {
+        // add css styling if operator button selected
         if (btnOperator[i].value === operatorType) {
             btnOperator[i].classList.add('selected');
         }
@@ -99,15 +119,15 @@ function selected() {
     }
 }
 
-
+// processes user input
 function clicked(input) {
     // no argument has been selected
     if (argument1.length === 0) {
-        // length of total is not 0, i.e., thera has been a previous calculation
+        // length of total is not 0, i.e., there has been a previous calculation
         if (total.length !== 0) {
             // input equals one of the operators or an equal sign
-            if (isOperator(input) ||
-                input === '=') {
+            // parses result of the previous calculation as the first argument 
+            if (isOperator(input) || input === '=') {
                 argument1 = [...total];
             }
             total.length = 0;
@@ -124,7 +144,7 @@ function clicked(input) {
             isNumber(argument1, input);
         }
         else if (input === '.') {
-            // the argument is not already a float and is less than 14 characters in lenth
+            // the argument is not already a float
             if (!float1 && argument1.length < maxLength - 1) {
                 argument1.push(input);
                 float1 = true;
@@ -142,7 +162,7 @@ function clicked(input) {
         else if (isOperator(input)) {
             operator = true;
             operatorType = input;
-            selected();
+            btnSelected();
         }
         return argument1.reduce((total, num) => total + num);
     }
@@ -154,8 +174,8 @@ function clicked(input) {
             isNumber(argument2, input);
         }
         else if (input === '.') {
-            // the argument is not already a float and is less than 14 characters in length
-            if (!float2 && argument2.length < maxLength - 1) {
+            // the argument is not already a float
+            if (!float2 && argument2.length < (maxLength - 1)) {
                 if (argument2.length === 0) {
                     argument2.push('0');
                     argument2.push(input);
@@ -171,19 +191,19 @@ function clicked(input) {
             if (argument2.length === 0) {
                 operator = false;
                 operatorType = '';
-                selected();
+                btnSelected();
                 return argument1.reduce((total, num) => total + num);
             }
             // the second argument has been selected
             else {
                 argument2.length = 0;
-                float1 = false;
+                float2 = false;
                 return 0;
             }
         }
         else if (input === 'reset') {
             reset();
-            selected();
+            btnSelected();
             return argument1;
         }
         else if (input === '=') {
@@ -193,7 +213,7 @@ function clicked(input) {
         else if (isOperator(input)) {
             if (argument2.length === 0) {
                 operatorType = input;
-                selected();
+                btnSelected();
                 return argument1.reduce((total, num) => total + num);
             }
             else {
@@ -209,7 +229,6 @@ function clicked(input) {
 
 // solves the mathematical expression 
 function equals() {
-
     let result;
     // if no second argument is selected the first argument is displayed
     if (argument2.length === 0) {
@@ -243,19 +262,28 @@ function equals() {
             result = divide(arg1, arg2);
     }
     reset();
-    total = [...result.toString()]; 
-    return result;
+    total = [...formatNumber(result).toString()];
+    return formatNumber(result);
 }
 
 // checks if input is a number
 function isNumber(argument, input) {
-    if (argument.length < maxLength) {
-        // the only character in the argument array is 0 (default)
-        if (argument.length === 1 && argument[0] === '0') {
-            argument[0] = input;
-        }
-        else {
+    // input is 0 and argument is a float
+    if (input === '0' && argument.indexOf('.') !== -1) {
+        // prevents from entering a float with 0 as the last digit
+        if (argument.length < maxLength - 1) {
             argument.push(input);
+        }
+    }
+    else {
+        if (argument.length < maxLength) {
+            // the only character in the argument array is 0 (default)
+            if (argument.length === 1 && argument[0] === '0') {
+                argument[0] = input;
+            }
+            else {
+                argument.push(input);
+            }
         }
     }
 }
@@ -275,10 +303,59 @@ function isOperator(input) {
 // resets all variables to default values 
 function reset() {
     argument1.length = 0;
-    float1 = false;
+    float1           = false;
     argument2.length = 0;
-    float1 = false;
-    operator = false;
-    operatorType = '';
-    selected();
+    float2           = false;
+    operator         = false;
+    operatorType     = '';
+    btnSelected();
+}
+
+
+// formats the number based on the size of the display
+function formatNumber(number) {
+    if (number.toString().length > maxLength) {
+        let formatedNr = number;
+        while (formatedNr.toString().length > maxLength) {
+            // number is a float
+            if (formatedNr % 1 !== 0) {
+                // prevents a float smaller than 10^(-maxLength) from being rounded to 0
+                if (formatedNr < Math.pow(10, -maxLength)) {
+                    formatNumber = number.toExponential();
+                }
+                else {
+                    // represents the minimum number of characters to display a float, i.e at least one digit before the decimal point and a decimal point  
+                    let i = 2;
+                    while (formatedNr.toString().length > maxLength) {
+                        formatedNr = parseFloat(formatedNr).toFixed(maxLength - i);
+                        i++;
+                        // the number is rounded to an integer but is still has more characters than maxLength
+                        if (maxLength - i === -1) {
+                            break;
+                        }
+                    }
+                }
+            }
+            // number is an integer
+            else {
+                // formats the number to exponential form
+                formatedNr = number.toExponential();
+            }
+
+            // if the length of the number formatted to exponential form still exceeds maxLength, the number of digits after the decimal point are specified 
+            if (formatedNr.length > maxLength) {
+                // represents the minimum number of characters to display an exponenet,
+                // i.e., a digit before decimal point, decimal point, Euler's number, the sign and power of the Euler's number 
+                let j = 5;
+                // the number of digits after decimal point is decreased until length of the formated number equals that of maxLength
+                while (formatedNr.length > maxLength) {
+                    formatedNr = number.toExponential(maxLength - j);
+                    j++;
+                }
+            }
+        }
+        return formatedNr;
+    } else {
+        return number;
+    }
 }
